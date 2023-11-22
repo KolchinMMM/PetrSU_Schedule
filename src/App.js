98c63c2459cfa04c75927286559fac22977ac25f
KolchinMMM <wilokhvost@mail.ru>
2023-11-22 16:59:18 +0300

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,4 +20,13 @@ function App() {
   );
 }
 
-export default App;
+function Nakakal() {
+  return (
+    <div>
+      Naval govna
+    </div>
+
+  );
+}
+
+export default Nakakal;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Nakakal from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Nakakal />
   </React.StrictMode>
 );
 
