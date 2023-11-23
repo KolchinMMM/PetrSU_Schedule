2aaa0ef9fc783f72c8b9148a7d7cef291d44e546
KolchinMMM <wilokhvost@mail.ru>
2023-11-23 18:37:19 +0300

    seems it doesn't work

--- src/App.js
+++ src/App.js
@@ -3,30 +3,50 @@ import './App.css';
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+}
+
+function ShowFilterMenu(){
+
+}
+
+function FilterMenu(){
+	return{}
+}
+
+let counter = 0;
+function MenuSandwich(){
+	return(
+	<div className="SandwichMenu" onClick={ShowFilterMenu}>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
+	);
 }
 
 function Main() {
-  return (
-    <div className="Main">
-      Навал говна
-    </div>
+	return (
+		<div className="Main">
+			Навал говна
+			<MenuSandwich />
+		</div>
 
-  );
+	);
 }
 
 export default Main;
